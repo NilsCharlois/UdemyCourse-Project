ca0281666b0caa959d4be79cb26507301f02dadf
NilsCharlois <nils.charlois@gmail.com>
2019-08-16 13:54:12 +0100

    add ingredient array to recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,35 @@
 import { EventEmitter } from '@angular/core'
 
 import { Recipe } from './recipe.model'
+import { Ingredient } from '../shared/ingredient.model'
 
 export class RecipeService {
 	recipeSelected = new EventEmitter<Recipe>()
 
 	private recipes: Recipe[] = [
-    new Recipe('A Test recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2017/09/17/23/21/gastronomy-2760200_960_720.jpg'),
-    new Recipe('A Test recipe 1', 'This is simply a test 1', 'https://cdn.pixabay.com/photo/2017/09/17/23/21/gastronomy-2760200_960_720.jpg')
+    new Recipe('Custard King',
+'https://e-liquid-recipes.com/recipe/202670/Custard%20King',
+'https://images-na.ssl-images-amazon.com/images/I/81R44Q7bxWL._SL1500_.jpg',
+[
+new Ingredient('PG dilutant', 0.56),
+new Ingredient('VG dilutant', 6.69),
+new Ingredient('Nicotine juice 48 mg (75/25 PG/VG)', 1.25),
+new Ingredient('RY4 Double (TPA)', 0.80),
+new Ingredient('Sweet Cream (TPA)', 0.30),
+new Ingredient('Vanilla Swirl (TPA)', 0.40)
+]
+),
+    new Recipe('Strawberry fog',
+'https://e-liquid-recipes.com/recipe/226322/strawberry%20fog',
+'https://images-na.ssl-images-amazon.com/images/I/81R44Q7bxWL._SL1500_.jpg',
+[
+new Ingredient('VG dilutant', 5.04),
+new Ingredient('Nicotine juice 36 mg (100% VG)', 44.00),
+new Ingredient('Bavarian Cream (TPA)', 1.80),
+new Ingredient('Cheesecake (Graham Crust) (TPA)', 3.0),
+new Ingredient('Strawberry (Ripe) (TPA)', 6.00),
+new Ingredient(' Vanilla Custard (TPA)', 1.20)
+])
   ];
 
   getRecipes() {
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,10 +1,16 @@
+import { EventEmitter } from '@angular/core'
 import { Ingredient } from '../shared/ingredient.model'
 
 
 export class ShoppingListService {
+	ingredientsChanged = new EventEmitter<Ingredient[]>()
+
 	private ingredients: Ingredient[] = [
-	    new Ingredient('Apples', 5),
-	    new Ingredient('Tomatoes', 10),
+	    new Ingredient('Bavarian Cream (TPA)', 1.80),
+	    new Ingredient('Cheesecake (Graham Crust) (TPA)', 3.00),
+	    new Ingredient('Cheesecake (Graham Crust) (TPA)', 3.00),
+	    new Ingredient('Strawberry (Ripe) (TPA)', 6.00),
+	    new Ingredient('Vanilla Custard (TPA)', 1.20),
        ];
 
 	getIngredients(){
@@ -13,5 +19,6 @@ export class ShoppingListService {
 
 	addIngredient(ingredient: Ingredient){
 		this.ingredients.push(ingredient)
+		this.ingredientsChanged.emit(this.ingredients.slice())
 	}
 }
